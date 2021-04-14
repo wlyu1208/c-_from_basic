f1038975203b5ee7c285f829c5d468e689668f3d
유원우 (Won Woo Lyu) 사원 (진단AI팀) <wwlyu@seegene.com>
2021-04-14 09:38:57 +0900

    add Hola Wolrd

---
+++ hello_world/hello_world/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // writeline automatically has "\n" 
+            Console.WriteLine("Hello World!");
+            Console.Write("Hello C#");
+        }
+    }
+}
