8ed60e578f1a82279ef4506df26a152796400e91
유원우 (Won Woo Lyu) 사원 (진단AI팀) <wwlyu@seegene.com>
2021-04-14 11:07:45 +0900

    hello world in window form having click

---
+++ 2_hello_world_win_form/hello_world_win_form/calculator.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace hello_world_win_form
+{
+    partial class calculator
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hello_label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // hello_label
+            // 
+            this.hello_label.AutoSize = true;
+            this.hello_label.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.hello_label.Font = new System.Drawing.Font("맑은 고딕", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.hello_label.Location = new System.Drawing.Point(12, 20);
+            this.hello_label.Name = "hello_label";
+            this.hello_label.Size = new System.Drawing.Size(130, 35);
+            this.hello_label.TabIndex = 0;
+            this.hello_label.Text = "Click Here";
+            this.hello_label.Click += new System.EventHandler(this.hello_click);
+            // 
+            // calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 259);
+            this.Controls.Add(this.hello_label);
+            this.Name = "calculator";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label hello_label;
+    }
+}
+
---
+++ 2_hello_world_win_form/hello_world_win_form/calculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hello_world_win_form
+{
+    public partial class calculator : Form
+    {
+        public calculator()
+        {
+            InitializeComponent();
+        }
+
+        /*
+         * Click "Clike Here" will change text to "Hello World"
+         */
+        private void hello_click(object sender, EventArgs e)
+        {
+            //Clicking "Click Here" changes text to "Hello world"
+
+            //hello_label.Text = "Hello World";
+
+
+            //Click changes text to sum of 3
+            int sum = 1 + 2;
+
+            hello_label.Text = sum.ToString();
+
+
+        }
+    }
+}
